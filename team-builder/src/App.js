f6707e73215c7b3d4b78ec01ba73accb43e22044
Mychael M <mychaelbmenges@gmail.com>
2020-09-06 17:23:11 -0600

    added static member list and created basic App.js page

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import memberList from './memberList';
 import './App.css';
 
 function App() {
+
+  const [members, setMembers] = useState(memberList);
+  console.log(members);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Members</h1>
       </header>
+      <div>
+        <h2>List Members</h2>
+      </div>
     </div>
   );
 }
---
+++ team-builder/src/memberList.js
@@ -0,0 +1,19 @@
+const members = [
+  {
+    id: 1,
+    name: 'Mychael Menges',
+    role: 'Web Dev Student'
+  },
+  {
+    id: 2,
+    name: 'Toneio',
+    role: 'Team Lead'
+  },
+  {
+    id: 3,
+    name: 'Kayan Williams',
+    role: 'Web Dev Student'
+  }
+]
+
+export default members;
